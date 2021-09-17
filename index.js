5847194714fd618163d1be8e789eb01ccdbd6670
Mikko Kokkonen <mikko.kokkonen@itsmycargo.com>
2021-09-17 20:08:17 +0200

    initial version

---
+++ index.js
@@ -0,0 +1,74 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+function sha(context) {
+  if (context.payload && context.payload.pull_request) {
+    return context.payload.pull_request.head.sha;
+  } else {
+    return context.sha;
+  }
+}
+
+function metaLabels(context) {
+  return [
+    `org.opencontainers.image.created=${(new Date()).toISOString()}`,
+    `org.opencontainers.image.description=${context.payload.repository.description || ''}`,
+    `org.opencontainers.image.revision=${sha(context) || ''}`,
+    `org.opencontainers.image.source=${context.payload.repository.html_url || ''}`,
+    `org.opencontainers.image.title=${context.payload.repository.name || ''}`,
+    `org.opencontainers.image.url=${context.payload.repository.html_url || ''}`,
+  ];
+}
+
+function metaTags(repository, context, commit) {
+  var tags = [];
+
+  tags.push(`${repository}:${commit}`);
+
+  if (context.ref == `refs/heads/${context.payload.repository.default_branch}`) {
+    tags.push(`${repository}:latest`)
+    tags.push(`${repository}:release-${commit.substr(0, 7)}`)
+  } else {
+    tags.push(`${repository}:dev-${commit.substr(0, 7)}`)
+  }
+
+  // Append PR number
+  if (context.payload && context.payload.pull_request) {
+    tags.push(`${repository}:pr-${context.payload.pull_request.number}`)
+  }
+
+  return tags;
+}
+
+try {
+  // Fetch inputs
+  // const repository = core.getInput('repository');
+
+  // Initialize context
+  // const labels = metaLabels(github.context);
+  // const tags = metaTags(
+  //   repository,
+  //   github.context,
+  //   sha(github.context)
+  // );
+  //
+  // console.log(`Labels:\n  ${labels.join(`\n  `)}`);
+  // core.setOutput('labels', labels.join(`\n`));
+  //
+  // console.log(`Tags:\n  ${tags.join(`\n  `)}`);
+  // core.setOutput('tags', tags.join(`\n`));
+  //
+  // // Accessors
+  // console.log(`repositoryTag: ${tags[0]}`);
+  // core.setOutput('repositoryTag', tags[0]);
+  // console.log(`repository: ${repository}`);
+  // core.setOutput('repository', repository);
+  // console.log(`tag: ${sha(github.context)}`);
+  // core.setOutput('tag', sha(github.context));
+
+  console.log(github);
+  console.log(github.context);
+  console.log(github.context.payload);
+} catch (error) {
+  core.setFailed(error.message);
+}
